0df2fc76cbfa07470cb3c4401510491fa96ef706
ian <jesperiansantos.2021@gmail.com>
2021-11-05 22:29:19 -0300

    enviando arquivos

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Carta from './Carta.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Carta remetente="Joana" destino="Minas Gerais" />
+    </div>
+  );
+}
+
+export default App;
---
+++ Carta.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default function Carta(props) {
+    return (
+        <div>
+            <h1> Olá { props.remetente } - { props.destino } </h1>
+            <br />
+            <br />
+            <span>Estou escrevendo essa carta por causa da <br />
+            atividade que os alunos vão fazer, okay. <br />
+            Não desiste, continue fazendo as atividades, treinando e praticando.<br />
+            O céu é o limite!</span>
+        </div>
+    );
+};
